f38450ef83f668196784b5a86edbd93a273c2a82
yongjun94 <37098848+yongjun94@users.noreply.github.com>
2023-09-01 20:15:56 +0900

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -1,30 +1,44 @@
-const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting");
+const nameContainer = document.querySelector(".greeting");
 
-const HIDDEN_CLASSNAME = "hidden";
-const USERNAME_KEY = "username";
+function paintName(name) {
+    nameContainer.innerHTML = "";
+    const title = document.createElement("span");
+    title.className = "name_text";
+    title.innerHTML = `Hello ${name}`;
+    nameContainer.appendChild(title);
+}
 
-function onLoginSubmit(event) {
+function handleSubmit(event){
     event.preventDefault();
-    const username = loginInput.value;
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    localStorage.setItem("username", username)
-    paintGreeting(username);
+    const form = event.target;
+    const input = form.querySelector("input");
+    const value = input.value;
+    localStorage.setItem("username", value);
+    paintName(value);
 }
 
-function paintGreeting(username) {
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+function paintInput() {
+    const input = document.createElement("input");
+    input.placeholder = "이름을 입력하세요";
+    input.type = "text";
+    input.className = "name_input";
+    const form = document.createElement("form");
+    form.addEventListener("submit", handleSubmit);
+    form.appendChild(input);
+    nameContainer.appendChild(form);
 }
 
-const savedUsername = localStorage.getItem(USERNAME_KEY);
-
-if(savedUsername === null){
-    //show form
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener("submit", onLoginSubmit);
-} else{
-    //show greeting
-    paintGreeting(savedUsername);
+function loadName() {
+    const name = localStorage.getItem("username");
+    if (name===null){
+        paintInput();
+    } else {
+        paintName(name);
+    }
 }
+
+function init() {
+    loadName();
+}
+
+init();
