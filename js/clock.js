ec5bb54f8662eb32239bf8b4340b803be77cc5eb
yongjun94 <37098848+yongjun94@users.noreply.github.com>
2023-09-01 20:15:43 +0900

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,13 +1,19 @@
-const clock = document.querySelector("h2#clock");
+const clock = document.querySelector(".clock .clock_text");
 
 function getClock(){
     const date = new Date();
     const hours =String(date.getHours()).padStart(2,"0");
     const minutes = String(date.getMinutes()).padStart(2,"0");
-    const seconds = String(date.getSeconds()).padStart(2,"0");
 
-    clock.innerText = `${hours}:${minutes}:${seconds}`;
+    clock.innerHTML = `${hours}:${minutes}`;
+
+    return;
 }
 
-getClock();
-setInterval(getClock, 1000);
+function init() {
+    getClock();
+    setInterval(getClock, 1000);
+    return;
+}
+
+init();
